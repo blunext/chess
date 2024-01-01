c8ab0cca67cf1dc05d161b0e0ca73f2f85266d93
Blunext <>
2024-01-01 14:27:52 +0100

    flat bitboards func

--- board/bitboard.go
+++ board/bitboard.go
@@ -1,3 +1,14 @@
+// Package board Layout 2: https://gekomad.github.io/Cinnamon/BitboardCalculator/
+//
+//	56	57	58	59	60	61	62	63
+//	48	49	50	51	52	53	54	55
+//	40	41	42	43	44	45	46	47
+//	32	33	34	35	36	37	38	39
+//	24	25	26	27	28	29	30	31
+//	16	17	18	19	20	21	22	23
+//	08	09	10	11	12	13	14	15
+//	00	01	02	03	04	05	06	07
+
 package board
 
 import (
@@ -9,7 +20,7 @@ type Bitboard uint64
 func BB() {
 	b := Bitboard(0x40201)
 	b.SetBit(27)
-	s := pretty(b)
+	s := b.Pretty()
 	fmt.Println(s)
 }
 
@@ -45,7 +56,7 @@ func squareIndex(f, r int) int {
 	return (r << 3) + f
 }
 
-func pretty(b Bitboard) string {
+func (b Bitboard) Pretty() string {
 	s := "+---+---+---+---+---+---+---+---+\n"
 	for r := Rank8; r >= Rank1; r-- {
 		for f := FileA; f <= FileH; f++ {
@@ -59,4 +70,12 @@ func pretty(b Bitboard) string {
 	}
 	s += "  a   b   c   d   e   f   g   h\n"
 	return s
+}
+
+func Flat(boards []Bitboard) Bitboard {
+	var flatten Bitboard
+	for _, b := range boards {
+		flatten = flatten | b
+	}
+	return flatten
 }
